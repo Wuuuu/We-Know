02e4c920c4d6913a0058d637334c1dea10300d88
WuYaoke <wuykjiu@163.com>
2023-06-29 12:33:12 +0800

    fix: 修复register使用字段错误

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -5,14 +5,14 @@ import prisma from "@/app/libs/prismadb";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const { email, username, password } = body;
+  const { email, name, password } = body;
 
   const hashedPassword = await bcrypt.hash(password, 12);
 
   const user = await prisma.user.create({
     data: {
       email,
-      username,
+      name,
       hashedPassword,
     },
   });
